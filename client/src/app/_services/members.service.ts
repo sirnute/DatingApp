fc84b2bf2326abbfc8c84eeeaa64c8ab3fd74627
sirnute <nuteempire@gmail.com>
2021-04-27 01:01:59 -0500

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -50,4 +50,14 @@ export class MembersService {
     )
   }
 
+  setMainpohto(photoId: number)
+  {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number)
+  {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
 }
